0c315f1a67407486ef3feb5b06d8f11234f59fd8
Sinhala Wal Katha <0752952894.ip@gmail.com>
2026-05-28 19:58:51 +0530

    Update Youtube.tsx

--- src/layouts/shortcodes/Youtube.tsx
+++ src/layouts/shortcodes/Youtube.tsx
@@ -13,8 +13,32 @@ const Youtube = ({
     import("@justinribeiro/lite-youtube");
   }, []);
 
-  // @ts-ignore
-  return <lite-youtube className="rounded-lg" videoid={id} videotitle={title} {...rest} />;
+  // YouTube thumbnail සහ embed URL එක ස්මාර්ට් විදිහට සකස් කිරීම
+  const thumbnailUrl = `https://img.youtube.com/vi/${id}/maxresdefault.jpg`;
+  const embedUrl = `https://www.youtube.com/embed/${id}`;
+
+  return (
+    <div 
+      className="youtube-wrapper my-4"
+      itemScope 
+      itemType="https://schema.org/VideoObject"
+    >
+      {/* 📊 Google Video SEO සඳහා අවශ්‍ය සැඟවුණු මෙටා ඩේටා */}
+      <meta itemProp="name" content={title} />
+      <meta itemProp="description" content={`${title} - Watch on Wal Katha`} />
+      <meta itemProp="thumbnailUrl" content={thumbnailUrl} />
+      <meta itemProp="embedUrl" content={embedUrl} />
+      <meta itemProp="uploadDate" content="2026-01-01T00:00:00+05:30" /> {/* Google Validator එකට අනිවාර්ය ටැගයක් */}
+
+      {/* @ts-ignore */}
+      <lite-youtube 
+        className="rounded-lg" 
+        videoid={id} 
+        videotitle={title} 
+        {...rest} 
+      />
+    </div>
+  );
 };
 
 export default Youtube;
