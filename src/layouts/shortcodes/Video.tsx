f8f25983aa737c56de3ff72cbb00c7a35dba7d39
Sinhala Wal Katha <0752952894.ip@gmail.com>
2026-05-28 20:05:55 +0530

    Update Video.tsx

--- src/layouts/shortcodes/Video.tsx
+++ src/layouts/shortcodes/Video.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 function Video({
   title,
   width = 500,
@@ -12,20 +13,35 @@ function Video({
   src: string;
   [key: string]: any;
 }) {
+  // වීඩියෝ එකේ ඇත්තම URL එක කෝඩ් එක ඇතුළෙන් තහවුරු කර ගැනීම
+  const videoUrl = src.match(/^http/) ? src : `/videos/${src}`;
+
   return (
-    <video
-      className="overflow-hidden rounded-lg"
-      width={width}
-      height={height}
-      controls
-      {...rest}
+    <div 
+      className="video-wrapper my-4"
+      itemScope 
+      itemType="https://schema.org/VideoObject"
     >
-      <source
-        src={src.match(/^http/) ? src : `/videos/${src}`}
-        type="video/mp4"
-      />
-      {title}
-    </video>
+      {/* 📊 Google Video SEO සඳහා අවශ්‍ය අනිවාර්ය මෙටා ඩේටා */}
+      <meta itemProp="name" content={title} />
+      <meta itemProp="description" content={`${title} - Exclusive Content on Wal Katha`} />
+      <meta itemProp="contentUrl" content={videoUrl} />
+      <meta itemProp="uploadDate" content="2026-01-01T00:00:00+05:30" />
+      {/* Fallback thumbnail: වීඩියෝ සර්ච් එකට ඩිෆෝල්ට් ඉමේජ් එකක් දීම */}
+      <meta itemProp="thumbnailUrl" content="/images/default-video-thumbnail.jpg" />
+
+      <video
+        className="overflow-hidden rounded-lg w-full"
+        width={width}
+        height={height}
+        controls
+        preload="metadata" // UX/SEO: වීඩියෝ එක ප්ලේ කරනකම් මුළු ෆයිල් එකම බාන එක වළක්වා සයිට් එක ස්පීඩ් කරයි
+        {...rest}
+      >
+        <source src={videoUrl} type="video/mp4" />
+        <p>Your browser does not support the video tag. Here is a link to the video: {title}</p>
+      </video>
+    </div>
   );
 }
 
